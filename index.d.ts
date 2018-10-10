5566e0259588d0381ad6f984d6c76c62fef0e607
Belmin Mustajbasic <belmin.mustajbasic@gmail.com>
2018-10-10 16:12:55 +0200

    Fixed return value of objectValidator definition

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@ export function isBoolean(object: any): boolean;
 export function isUrl(object: any, config: any): boolean;
 export function isEmail(object: any, config: any): boolean;
 export function isArray(object: any, config: any): boolean;
-export function objectValidator(properties: any): boolean;
+export function objectValidator(properties: any): (object: any)=>boolean;
 export function stringValidator(isRequired: boolean, config: any): any;
 export function numberValidator(isRequired: boolean, config: any): any;
 export function booleanValidator(isRequired: boolean, config: any): any;
