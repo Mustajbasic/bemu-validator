a075dd480c3eb5915f083bfa9303e1c98cf0d6b0
Belmin Mustajbasic <belmin.mustajbasic@gmail.com>
2018-10-10 16:03:21 +0200

    Added typescript definitions, incmplete

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+
+export function isString(object: any, config: any): boolean;
+export function isNumber(object: any, config: any): boolean;
+export function isBoolean(object: any): boolean;
+export function isUrl(object: any, config: any): boolean;
+export function isEmail(object: any, config: any): boolean;
+export function isArray(object: any, config: any): boolean;
+export function objectValidator(properties: any): boolean;
+export function stringValidator(isRequired: boolean, config: any): any;
+export function numberValidator(isRequired: boolean, config: any): any;
+export function booleanValidator(isRequired: boolean, config: any): any;
+export function urlValidator(isRequired: boolean, config: any): any;
+export function emailValidator(isRequired: boolean, config: any): any;
+export function arrayValidator(isRequired: boolean, config: any): any;
+export function customValidator(isRequired: boolean, isValid: any, config: any): any;
+export function bundleWithConfig(validator: any, config: any): any;
