ff423787e6bcc29b8218952c8c7365e8930b530b
Belmin Mustajbasic <belmin@Belmins-MacBook-Pro.local>
2019-05-29 09:41:03 +0200

    Updated index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,7 @@ export function isBoolean(object: any): boolean;
 export function isUrl(object: any, config: any): boolean;
 export function isEmail(object: any, config: any): boolean;
 export function isArray(object: any, config: any): boolean;
+export function isDate(object: any, config: any): boolean;
 export function objectValidator(properties: any): (object: any)=>boolean;
 export function stringValidator(isRequired: boolean, config: any): any;
 export function numberValidator(isRequired: boolean, config: any): any;
